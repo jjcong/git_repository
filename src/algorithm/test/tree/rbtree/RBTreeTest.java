1476866d8fffa150e109587be322e6589a3c5d23
jjcong <jincong00@163.com>
2018-03-16 18:52:07 +0800

    二叉平衡树

--- src/algorithm/test/tree/rbtree/RBTreeTest.java
+++ src/algorithm/test/tree/rbtree/RBTreeTest.java
@@ -7,7 +7,7 @@ package algorithm.test.tree.rbtree;
  * @version V1.0.0
  * @param <T>
  */
-public class RBTreeTesst {
+public class RBTreeTest {
 
 	    private static final int a[] = {10, 40, 30, 60, 90, 70, 20, 50, 80};
 	    private static final boolean mDebugInsert = false;    // "插入"动作的检测开关(false，关闭；true，打开)
